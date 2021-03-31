bf3c077340c98b0dc26ec565ffff6a88e9dbafe6
Alice Viccajee <alice@fanatic.co.uk>
2021-03-31 15:17:18 +0100

    mobile view

---
+++ js/index.js
@@ -0,0 +1,36 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "flex";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  var trigger = document.getElementById('trigger');
+	var element = document.getElementById('mobile_nav');
+	trigger.addEventListener('click', () => {
+		element.classList.toggle('mobile-menu-open');
+    trigger.classList.toggle('menu-open');
+	});
+});
